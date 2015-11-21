6c54b49d2b8ddad280bc02f5e9ae96d0482f0d07
Jake Seeber <zippyjake1@gmail.com>
2015-11-21 11:48:07 -0500

    save

---
+++ public/js/boot.js
@@ -0,0 +1,21 @@
+// State: Flappy.Boot
+
+'use strict';
+
+var Flappy = { };
+
+Flappy.Boot = function () {};
+
+Flappy.Boot.prototype = {
+
+  init: function () {
+  },
+
+  preload: function () {
+  },
+
+  create: function () {
+    this.game.stage.backgroundColor = "#008001";
+  }
+
+};
